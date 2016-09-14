0674a7f01fc5b5718fa398e816d91b9645fcf871
greatontime <pathomphong1144@gmail.com>
2016-09-15 00:14:30 +0700

    Add new components

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ module.exports = {
     alias: {
       Main: 'app/components/main.jsx',
       applicationStyles: 'app/styles/app.scss',
-      Navigation: 'app/components/Navigation.jsx'
+      Navigation: 'app/components/Navigation.jsx',
+      Timer: 'app/components/Timer.jsx',
+      Countdown: 'app/components/Countdown.jsx'
     },
     extensions: ['','.js','.jsx']
   },
